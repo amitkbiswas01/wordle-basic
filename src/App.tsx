576c6ff981449a11c49e421a8f6cee76d753d563
Amit Kumar Biswas <amitkbiswas01@gmail.com>
2024-08-02 15:31:37 +0100

    feat: add GuessInput component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { TModal } from "./types";
 import Header from "./components/Header";
 import Modal from "./components/Modal";
+import GuessInput from "./components/GuessInput";
 
 function App() {
   const [modal, setModal] = React.useState<TModal>({
@@ -10,9 +11,10 @@ function App() {
   });
 
   return (
-    <div className="w-full max-w-[540px] mx-auto relative flex flex-col items-center">
+    <div className="w-full max-w-[540px] mx-auto relative flex flex-col items-center gap-4">
       {modal.isVisible && <Modal modal={modal} setModal={setModal} />}
       <Header setModal={setModal} />
+      <GuessInput />
     </div>
   );
 }
--- src/components/GuessInput.tsx
+++ src/components/GuessInput.tsx
@@ -1,7 +1,64 @@
 import React from "react";
 
 function GuessInput() {
-  return <div>GuessInput</div>;
+  const [guessError, setGuessError] = React.useState<string>("");
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.target as HTMLFormElement);
+    const guess = (formData.get("guess-input") as string).toUpperCase();
+
+    if (!guess) {
+      setGuessError("Please enter a guess.");
+      return;
+    }
+
+    if (guess.length < 5) {
+      setGuessError("Minimum word length is 5 characters.");
+      return;
+    }
+
+    if (guess.length > 5) {
+      setGuessError("Maximum word length is 5 characters.");
+      return;
+    }
+
+    setGuessError("");
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className="w-full p-4 bg-sky-100 rounded-md flex flex-col justify-center items-start gap-2"
+      aria-label="Submit your guess"
+    >
+      <label className="text-lg" htmlFor="guess-input">
+        Enter your guess:
+      </label>
+      <input
+        type="text"
+        id="guess-input"
+        name="guess-input"
+        className="w-full p-4 uppercase border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500"
+        aria-describedby="guess-input-help"
+        aria-invalid={!!guessError}
+        aria-errormessage="guess-input-error"
+      />
+      {guessError && (
+        <p
+          id="guess-input-error"
+          className="w-full p-4 rounded-md text-white bg-red-600"
+          role="alert"
+        >
+          {guessError}
+        </p>
+      )}
+      <p id="guess-input-help" className="sr-only">
+        Enter a 5-letter word.
+      </p>
+    </form>
+  );
 }
 
 export default GuessInput;
